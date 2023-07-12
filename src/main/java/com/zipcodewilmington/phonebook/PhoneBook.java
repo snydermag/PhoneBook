84a78f001df8d8602239224babb4f56b3af4488d
maggie <snydermag@gmail.com>
2023-07-12 17:39:08 -0400

    Constructor tests pass

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,11 +13,12 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this(new TreeMap<>());
+
     }
 
     public void add(String name, String phoneNumber) {
@@ -30,6 +28,10 @@ public class PhoneBook {
     }
 
     public void remove(String name) {
+    }
+
+    public Boolean hasEntry(String name, String number) {
+        return null;
     }
 
     public Boolean hasEntry(String name) {
@@ -49,6 +51,6 @@ public class PhoneBook {
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return new HashMap(phonebook);
     }
 }
